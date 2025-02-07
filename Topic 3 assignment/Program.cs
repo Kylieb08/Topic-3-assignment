0a98c53501b991b0b9eb527c7a05cd2b79f34b22
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-07 14:26:09 -0500

    Add project files.

---
+++ Topic 3 assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_3_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string courseName;
+
+            //Assigning values
+            courseName = "Computer Science";
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
