dcb6887b0e90c8f5bcff1b7177ed3daa394821ec
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-07 14:42:54 -0500

    finished part one of the assignment

--- Topic 3 assignment/Program.cs
+++ Topic 3 assignment/Program.cs
@@ -17,19 +17,22 @@
             fakeRent = 2.99;
             realRent = 4000;
             room = 29;
-            name = "Kylie";
+            name = "Billyert";
             placeOfResidence = "New York";
             rats = 2;
             roomates = 3;
             dinner = "pizza";
 
+            Console.WriteLine("This is part one of the assignment. It is best read in a New York accent");
+            Console.WriteLine("");
             Console.WriteLine("Hello, my name is " + name + ".");
             Console.WriteLine("I live in " + placeOfResidence + ".");
-            Console.WriteLine("I have " + roomates + ", " + rats + " of them are rats.");
+            Console.WriteLine("I have " + roomates + " roomates, " + rats + " of them are rats.");
             Console.WriteLine("We eat " + dinner + " for dinner every night.");
             Console.WriteLine("Da rent? " + fakeRent + " a month.");
-            Console.WriteLine("No, I lied, it's actually " + realRent + ". A second");
-            
+            Console.WriteLine("No, I lied, it's actually " + realRent + " dollars. A second");
+            Console.WriteLine("Ya know I'm considering getting into a " + courseName + " course to learn programming");
+            Console.WriteLine("Maybe if I can get a career in that rent will be affordable.");
         }
     }
 }
