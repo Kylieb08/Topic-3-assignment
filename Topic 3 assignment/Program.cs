2a6b770a57d1515cf198261993e0516ad7bdcf2b
Kylie Banman <339780520@GoTVDSB.ca>
2025-02-07 14:36:53 -0500

    complaining about rent in New York

--- Topic 3 assignment/Program.cs
+++ Topic 3 assignment/Program.cs
@@ -4,13 +4,32 @@
     {
         static void Main(string[] args)
         {
+            //Kylie Banman
+
+
             //Variables
-            string courseName;
+            string courseName, name, placeOfResidence, dinner;
+            double fakeRent, realRent;
+            int room, rats, roomates;
 
             //Assigning values
             courseName = "Computer Science";
+            fakeRent = 2.99;
+            realRent = 4000;
+            room = 29;
+            name = "Kylie";
+            placeOfResidence = "New York";
+            rats = 2;
+            roomates = 3;
+            dinner = "pizza";
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, my name is " + name + ".");
+            Console.WriteLine("I live in " + placeOfResidence + ".");
+            Console.WriteLine("I have " + roomates + ", " + rats + " of them are rats.");
+            Console.WriteLine("We eat " + dinner + " for dinner every night.");
+            Console.WriteLine("Da rent? " + fakeRent + " a month.");
+            Console.WriteLine("No, I lied, it's actually " + realRent + ". A second");
+            
         }
     }
 }
